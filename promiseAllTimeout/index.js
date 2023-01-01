af2c16cb1d2b34d65fac70e7153bb2542c488947
yair-tawil-walkme <yair.tawil@walkme.com>
2023-01-01 12:42:44 +0200

    promise timeout

--- promiseAllTimeout/index.js
+++ promiseAllTimeout/index.js
@@ -1,19 +1,37 @@
 const timeoutPromise = (timeout) => new Promise((resolve) => {
     setTimeout(() => {
-        resolve(`done after ${timeout / 1000}s`)
+        console.log(`done after ${timeout / 1000}s`)
+        resolve(timeout)
     }, timeout)
-})
+});
 
 const promise1 = () => timeoutPromise(4000);
 const promise2 = () => timeoutPromise(3000);
 const promise3 = () => timeoutPromise(2000);
 const promise4 = () => timeoutPromise(1000);
 
-Promise.all([promise1(), promise2(), promise3(), promise4()]).then((value) => console.log(value))
+async function _logPromiseDuration(promise) {
+    const now = Date.now();
 
-//implement me
-// function MyPromiseAll(promises) {}
+    await promise
 
-// MyPromiseAll([promise1, promise2, promise3, promise4]).then((values) =>
-//   values.map(({ buf }) => console.log(buf.toString()))
-// );
+    console.log(`promise duration:`, Date.now() - now)
+}
+
+function promiseAll(promises) {
+    return Promise.all(promises)
+}
+
+function MyPromiseAll(promises) {
+
+}
+
+async function run() {
+    // comment
+    await _logPromiseDuration(promiseAll([promise1(), promise2(), promise3(), promise4()]).then((value) => console.log(value)))
+
+    // uncomment
+    // await logPromiseDuration(MyPromiseAll([promise1(), promise2(), promise3(), promise4()]).then((value) => console.log(value)))
+}
+
+run();
