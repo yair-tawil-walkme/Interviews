2fde909aec70f0076d4aeb6a565900ddc8f354af
inbal-hipsh <63241206+inbal-hipsh@users.noreply.github.com>
2023-09-05 15:02:18 +0300

    Update App.tsx remove comment

--- react-table/src/components/App.tsx
+++ react-table/src/components/App.tsx
@@ -3,7 +3,6 @@ import Header from './Header'
 import Table from './Table/Table'
 import { getRows } from '../db/rows'
 
-// Hi, I added a zip of the code because I had authentication issues that took too much time to solve...
 const App = () => {
   const [rows] = useState(getRows())
   const [displayRows, setDisplayRows] = useState(rows)
